019cfa29eb6ef7f4572510a43d26adf1e037fd67
Bobness <nope@nope.com>
2015-05-05 21:02:20 +0200

    Added author, fixed README.md

--- defines.h
+++ defines.h
@@ -1,4 +1,6 @@
 /*
+	Author bobness09, Mail bobnessdev@gmail.com
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
